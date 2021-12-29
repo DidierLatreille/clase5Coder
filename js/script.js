ee6e9471e326a141c3b705aa97b2a6b00fe6ccf0
DidierLatreille <didier.latreille@gmail.com>
2021-12-28 22:13:48 -0300

    terminado

--- js/script.js
+++ js/script.js
@@ -1,16 +1,50 @@
 class procesador{
-    constructor(modeloProce,coolerProce,precioTotal){
+    constructor(modeloProce,precioTotal){
         this.modelo = modeloProce;
-        this.cooler = coolerProce;
         this.precio = precioTotal;
+    }
+
+    calcularPrecio(){
+        switch (this.modelo){
+            case 'Ryzen 5 2600':
+                this.precio = 22000;
+                break;
+            case 'Ryzen 5 3600':
+                this.precio = 36000;
+                break;
+            case 'Ryzen 5 5600':
+                this.precio = 43000;
+                break;
+            default: alert("algo hicimos mal");
+        }
     }
 }
 
 function pedirModProce(){
-    let proce = prompt("Elige un procesador:\n" + "1. Ryzen 5 2600\n" + "2. Ryzen 5 3600\n" + "3. Ryzen 5 5600\n" + "4. Salir del programa");
+    let opcionMenuProce;
+    let modProce;
+    do {opcionMenuProce = parseInt(prompt("Elige un procesador:\n" + "1. Ryzen 5 2600 ($22.000)\n" + "2. Ryzen 5 3600($30.000)\n" + "3. Ryzen 5 5600(43.000)"));}
+    while (opcionMenuProce < 1 || opcionMenuProce > 3 || Number.isNaN(opcionMenuProce));
+
+    switch(opcionMenuProce){
+        case 1:
+            modProce = "Ryzen 5 2600";
+            break;
+        case 2:
+            modProce = "Ryzen 5 3600";
+            break;
+        case 3:
+            modProce = "Ryzen 5 5600";
+            break;
+        default: alert("algo hicimos mal");
+    }
+    return modProce;
 }
 
 let modeloProceElegido = pedirModProce();
-let coolerProceElegido = menuCooler();
 
-const procesadorElegido = new procesador(modeloProceElegido,coolerProceElegido);
+const procesadorElegido = new procesador(modeloProceElegido);
+
+procesadorElegido.calcularPrecio();
+
+console.log(procesadorElegido);
